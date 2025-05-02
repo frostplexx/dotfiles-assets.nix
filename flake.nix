31d772e98c173c5c352230fb25e8c0f0450b6094
daniel <daniel.inama02@gmail.com>
2025-05-02 08:34:40 +0200

    init commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+    description = "Wallpaper Storage";
+    inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    inputs.flake-utils = {
+        url = "github:numtide/flake-utils";
+        inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    outputs = {
+        nixpkgs,
+        flake-utils,
+        ...
+        }:
+        flake-utils.lib.eachDefaultSystem (
+            system: let
+                pkgs = import nixpkgs {
+                    inherit system;
+                    config = {allowUnfree = true;};
+                };
+
+
+            in {
+                devShells.default = pkgs.mkShell {
+                    packages = with pkgs; [
+                        git-lfs
+                    ];
+                    shellHook = ''
+                    '';
+                };
+            }
+        );
+}
